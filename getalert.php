4ffbef0fce98364caec3e7f3c5e3ad7419b0fccc
artrief <artrief.site@gmail.com>
2018-09-26 03:23:23 +0700

    Update getalert.php

--- getalert.php
+++ getalert.php
@@ -1,8 +1,1 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '[{"actif":"0","lien":"","message_dialog":"handasa","yes_btn":"","no_btn":"","all_user":"0"}]'; ?> 
- </body>
-</html>
+[{"actif":"0","lien":"","message_dialog":"handasa","yes_btn":"","no_btn":"","all_user":"0"}]
